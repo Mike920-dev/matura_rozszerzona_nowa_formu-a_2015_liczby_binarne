c5b64736dd9a651baa751341d93c7f691073207b
Mike-920-dev <65869511+Mike920-dev@users.noreply.github.com>
2021-01-09 14:25:37 +0100

    zadanie_1.py

--- zadanie_2.py
+++ zadanie_2.py
@@ -8,13 +8,13 @@ podzielna_przez_2 = 0
 podzielna_przez_8 = 0
 
 for liczba in liczby:
-    if liczba[-3:] == "000":
+    if liczba[-1] == '0' and liczba[-2] == '0' and liczba[-3] == '0':
         podzielna_przez_8 += 1
         podzielna_przez_2 += 1
-    elif liczba[-1] == "0":
+    elif liczba[-1] == '0':
         podzielna_przez_2 += 1
 
-print(podzielna_przez_2, " ", podzielna_przez_8)
+print(podzielna_przez_2, ' ', podzielna_przez_8)
 
 with open("wynik4.txt", "a", encoding="UTF-8") as plik:
     plik.write("b) " + str(podzielna_przez_2) + " " + str(podzielna_przez_8) + "\n")
