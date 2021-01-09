29edf9748e8d9386be41c86be08136ae0b073382
Mike-920-dev <65869511+Mike920-dev@users.noreply.github.com>
2021-01-09 16:08:28 +0100

    Add files via upload

---
+++ zadanie_3_v2.py
@@ -0,0 +1,17 @@
+with open("slowa_2018.txt", "r", encoding="UTF-8") as plik:
+    slowa = []
+    for line in plik:
+        slowa.append(line.split())
+
+liczba_wierszy = 0
+
+for slowo1, slowo2 in slowa:
+    slowo1 = "".join(sorted(slowo1))
+    slowo2 = "".join(sorted(slowo2))
+    if slowo1 == slowo2:
+        liczba_wierszy += 1
+
+print("c) Liczba wierszy zawierających parę słów, gdzie jedno słowo jest anagramem drugiego:", liczba_wierszy)
+
+with open("wyniki6.txt", "a", encoding="UTF-8") as plik:
+    plik.write("c) Liczba wierszy zawierających parę słów, gdzie jedno słowo jest anagramem drugiego: " + str(liczba_wierszy) + "\n")
